03486e969afba80e72ae6b9a08145b24ec73e0b6
Olivier Baltus <olivier.baltus@nsi.lu>
2022-09-15 16:48:46 +0200

    dd

--- demo-eds.js
+++ demo-eds.js
@@ -23,7 +23,8 @@ app.post('/type/', function (req, res) {
    });
 })
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+
+   var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+   var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
    console.log("EDS-demo service listening at http://%s:%s", host, port)
 })
